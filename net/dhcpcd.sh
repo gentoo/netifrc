gentoo/netifrc

commit bd8ff4aa89e1d56e9886dad32bb2406c8f43c747

Adding the saved args to the stop command
Closes: https://bugs.gentoo.org/881039
Signed-off-by: Kevin Martin <kevinmbecause@gmail.com>
(cherry picked from commit 0e1c19959a6d82accdb57fa7a1f5b0036037a001)
Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>
Closes: https://github.com/gentoo/netifrc/pull/41

net/dhcpcd.sh
@@ -90,8 +90,8 @@ dhcpcd_stop()
 	eval opts=\$dhcp_${IFVAR}
 	[ -z "${opts}" ] && opts=${dhcp}
 	case " ${opts} " in
-		*" release "*) dhcpcd -k "${IFACE}" ;;
-		*) dhcpcd -x "${IFACE}" ;;
+		*" release "*) dhcpcd -k "${args}" "${IFACE}" ;;
+		*) dhcpcd -x "${args}" "${IFACE}" ;;
 	esac
 	[ -f "${argsfile}" ] && rm -f "${argsfile}"
 	eend $?

commit bf02831a7a59f5552c916dde7dd47296a7bce0b1

dhcpcd: Fixing the handling of dhcpcd arguments with spaces
Bug: https://bugs.gentoo.org/881039
Signed-off-by: Kevin Martin <kevinmbecause@gmail.com>
Fixes: bd8ff4aa89e1d56e9886dad32bb2406c8f43c747
Closes: https://github.com/gentoo/netifrc/pull/48
Signed-off-by: Sam James <sam@gentoo.org>

net/dhcpcd.sh
@@ -90,8 +90,8 @@ dhcpcd_stop()
 	eval opts=\$dhcp_${IFVAR}
 	[ -z "${opts}" ] && opts=${dhcp}
 	case " ${opts} " in
-		*" release "*) dhcpcd -k "${args}" "${IFACE}" ;;
-		*) dhcpcd -x "${args}" "${IFACE}" ;;
+		*" release "*) dhcpcd -k ${args} "${IFACE}" ;;
+		*) dhcpcd -x ${args} "${IFACE}" ;;
 	esac
 	[ -f "${argsfile}" ] && rm -f "${argsfile}"
 	eend $?