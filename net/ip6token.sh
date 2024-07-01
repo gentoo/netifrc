bde6dd2ad5b393e351ae8fd8d89f3bec62e52914
Jaco Kroon <jaco@uls.co.za>
2024-07-01 16:50:33 +0200

    Implement ip-token(8) addressing for IPv6.
    This enables setting ip-token(8) up during pre-up from a config variable
    ip6token_${IFVAR} to set up tokens to be used during SLAAC
    auto-configuration.
    
    Closes: https://bugs.gentoo.org/935280
    Signed-off-by: Jaco Kroon <jaco@uls.co.za>

---
+++ net/ip6token.sh
@@ -0,0 +1,25 @@
+# Copyright (c) 2024 Gentoo Authors
+
+ip6token_depend()
+{
+	program ip
+	after interface
+}
+
+_config_vars="$_config_vars ip6token"
+
+ip6token_pre_start()
+{
+	local tconfig
+	eval tconfig=\$ip6token_${IFVAR}
+
+	[ -z "${tconfig}" ] && return 0
+	ip token set "${tconfig}" dev "${IFACE}" 
+	return $?
+}
+
+ip6token_post_stop()
+{
+	ip token del dev "${IFACE}"
+	return $?
+}
