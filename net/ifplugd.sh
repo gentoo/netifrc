28998b96e18c8d342c6896a3133169b20b0f305a
Alon Bar-Lev <alon.barlev@gmail.com>
2024-10-27 14:44:21 +0200

    net: ifplugd: support busybox
    busybox ifplugd does not support long options.
    
    Signed-off-by: Alon Bar-Lev <alon.barlev@gmail.com>
    Closes: https://github.com/gentoo/netifrc/pull/59
    Signed-off-by: Sam James <sam@gentoo.org>

--- net/ifplugd.sh
+++ net/ifplugd.sh
@@ -47,7 +47,7 @@ ifplugd_pre_start()
 
 	# Start ifplugd
 	eval start-stop-daemon --start --exec ifplugd \
-		--pidfile "${pidfile}" -- "${args}" --iface="${IFACE}"
+		--pidfile "${pidfile}" -- "${args}" -i "${IFACE}"
 	eend $? || return 1
 
 	eindent
